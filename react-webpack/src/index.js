3cef0b3258f7efad968e64cf956fcae05b1d6255
EmpAsh938 <ashokthapa938@gmail.com>
2021-05-11 11:01:07 +0545

    new react webpack

---
+++ react-webpack/src/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+
+const App = () => {
+  return (
+    <div>
+      <h1>Hello React from Webpack</h1>
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
